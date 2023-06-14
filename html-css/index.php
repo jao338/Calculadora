2efd8125357a62ac8051b01b69ac72d705f1869b
Jão <joaohenriquerc123@gmail.com>
2023-06-14 10:29:54 -0300

    Criação do projeto

---
+++ html-css/index.php
@@ -0,0 +1,29 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="../img-icon/calc/16x16.png">
+</head>
+<body>
+    
+    <main>
+
+        <div class="calculadora">
+            <div class="topo">
+                
+                <img src="../img-icon/calc/512x512.png" alt="icon" class="img-calc">
+                
+                <h2 class="titulo">Calculadora</h2>
+                
+                <button class="min"><img src="../img-icon/minus/512x512.png" alt="min" class="icon-min" width="16px"/></button>
+                <button class="max"><img src="../img-icon/square/512x512.png" alt="min" class="icon-min" width="16px"/></button>
+                <button class="close"><img src="../img-icon/close/512x512.png" alt="min" class="icon-min" width="16px"/></button>
+            </div>
+           
+        </div>
+
+    </main>
+</body>
+</html>
