ffc4972c2898cab0e74658417094500b1bd6b07d
Jão <joaohenriquerc123@gmail.com>
2023-06-28 11:05:55 -0300

    Aparentemente deu certo

--- html-css/index.php
+++ html-css/index.php
@@ -16,11 +16,10 @@
 
                 <!--<img src="../img-icon/calc/512x512.png" alt="icon" class="img-calc"> -->
             
-                <div class="topo-inicio">
-                    <h2 class="titulo">Calculadora</h2>
-                </div>
+                <h2 class="titulo">Calculadora</h2>
+
                 
-                <div class="topo-fim">
+                <div class="teste">
                     <button class="minus"><img src="../img-icon/minus/512x512.png" alt="min" class="icon-minus" width="16px"/></button>                    
                     <button class="square"><img src="../img-icon/square/512x512.png" alt="max" class="icon-square" width="16px"/></button>
                     <button class="close"><img src="../img-icon/close/512x512.png" alt="close" class="icon-close" width="16px"/></button>
