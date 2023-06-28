46ccb65292c7dcc1d11683ff7caa807401c0c489
Jão <joaohenriquerc123@gmail.com>
2023-06-28 11:02:42 -0300

    Mudando o alinhamento

--- html-css/index.php
+++ html-css/index.php
@@ -13,13 +13,20 @@
         <div class="calculadora">
             <div class="topo">
                 
-                <img src="../img-icon/calc/512x512.png" alt="icon" class="img-calc">
+
+                <!--<img src="../img-icon/calc/512x512.png" alt="icon" class="img-calc"> -->
+            
+                <div class="topo-inicio">
+                    <h2 class="titulo">Calculadora</h2>
+                </div>
                 
-                <h2 class="titulo">Calculadora</h2>
-                
-                <button class="min"><img src="../img-icon/minus/512x512.png" alt="min" class="icon-min" width="16px"/></button>
-                <button class="max"><img src="../img-icon/square/512x512.png" alt="min" class="icon-min" width="16px"/></button>
-                <button class="close"><img src="../img-icon/close/512x512.png" alt="min" class="icon-min" width="16px"/></button>
+                <div class="topo-fim">
+                    <button class="minus"><img src="../img-icon/minus/512x512.png" alt="min" class="icon-minus" width="16px"/></button>                    
+                    <button class="square"><img src="../img-icon/square/512x512.png" alt="max" class="icon-square" width="16px"/></button>
+                    <button class="close"><img src="../img-icon/close/512x512.png" alt="close" class="icon-close" width="16px"/></button>
+                </div>
+
+
             </div>
            
         </div>
