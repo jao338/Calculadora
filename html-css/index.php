c72a4fd4cea4e3ca155086ac525e152f975d5fc2
Jão <joaohenriquerc123@gmail.com>
2023-07-16 21:58:23 -0300

    Começo

--- html-css/index.php
+++ html-css/index.php
@@ -1,5 +1,46 @@
-<?php include'../php/calculadora.php'?>
+<?php 
 
+    include'../php/calculadora.php';
+
+    $calc = new calculadora();
+
+    $n1 = null;
+    $n2 = null;
+    $op = "1";
+    $res = null;
+
+    if(isset($_GET['op'])){
+       
+        $n1 = $_GET['num1'];
+        $n2 = $_GET['num2'];
+        $op = $_GET['op'];
+
+        switch($op){
+            case '1':
+
+                $res = $calc->somar($n1, $n2);
+                
+                break;
+            case '2':
+
+                $res = $calc->subtrair($n1, $n2);
+                    
+                break;
+            case '3':
+
+                $res = $calc->multiplicar($n1, $n2);
+    
+                break;
+            case '4':
+    
+                $res = $calc->dividir($n1, $n2);
+                        
+                break;
+        }
+
+    }
+
+?>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
@@ -11,54 +52,27 @@
 <body>
     
     <main>
+        <div id="calc">
 
-        <div class="calculadora">
-            <div class="topo">
-                <!--<img src="../img-icon/calc/512x512.png" alt="icon" class="img-calc"> -->
-            
-                <h2 class="titulo">Calculadora</h2>
+            <form action="index.php" method="GET">
+                <label for="num1">Primeiro número: </label>
+                <input type="number" name="num1" value= <?php echo $n1?> required>
 
-                <div class="topo-fim">
-                    <button class="minus"><img src="../img-icon/minus/512x512.png" alt="min" class="icon-minus" width="16px"/></button>                    
-                    <button class="square"><img src="../img-icon/square/512x512.png" alt="max" class="icon-square" width="16px"/></button>
-                    <button class="close"><img src="../img-icon/close/512x512.png" alt="close" class="icon-close" width="16px"/></button>
-                </div>
-            </div>
+                <label for="num2">Segundo número: </label>
+                <input type="number" name="num2" value= <?php echo $n2?> required>
 
-            
-            <div class="visor">
-                <textarea name="visor" id="visor" cols="65" rows="10"></textarea>
-            </div>
-            
+                <select name="op" id="op">
+                    <option value="1"><?php ($op=='1')?'Selected':'';?>Somar</option>
+                    <option value="2"><?php ($op=='2')?'Selected':'';?>Subtrair</option>
+                    <option value="3"><?php ($op=='3')?'Selected':'';?>Multiplicar</option>
+                    <option value="4"><?php ($op=='4')?'Selected':'';?>Dividir</option>
+                </select>
+                <input type="submit" name="bnt-igual" value="=" id="btn-igual">
+            </form>
+            <p>Resultado: <?php echo $res ?></p>
 
-            <div class="buttons">
-                <form action="index.php">
-
-                    <button type="submit" class="btn-num" id="btn-0">0</button>
-                    <button type="submit" class="btn-num" id="btn-1">1</button>
-                    <button type="submit" class="btn-num" id="btn-2">2</button>
-                    <button type="submit" class="btn-num" id="btn-3">3</button>
-                    <button type="submit" class="btn-num" id="btn-4">4</button>
-                    <button type="submit" class="btn-num" id="btn-5">5</button>
-                    <button type="submit" class="btn-num" id="btn-6">6</button>
-                    <button type="submit" class="btn-num" id="btn-7">7</button>
-                    <button type="submit" class="btn-num" id="btn-8">8</button>
-                    <button type="submit" class="btn-num" id="btn-9">9</button>
-
-                    <button class="btn-op">+</button>
-                    <button class="btn-op">-</button>
-                    <button class="btn-op">x</button>
-                    <button class="btn-op">/</button>
-
-                    <button class="btn-equal">=</button>
-                </form>
-            </div>
-
-            <div class="base">
-                <h5 style="display: block;">Copyright</h5>
-            </div>
         </div>
-
+        
     </main>
 </body>
 </html>
